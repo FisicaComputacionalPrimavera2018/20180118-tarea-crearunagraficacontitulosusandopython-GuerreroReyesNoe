144f0357ce16c6fcdfe52bb0bbea1e498133e619
GuerreroReyesNoe <35306754+GuerreroReyesNoe@users.noreply.github.com>
2018-01-22 11:34:00 -0600

    Add files via upload
    trabajo del 22 de enero

---
+++ graf.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+plt.plot([11,13,14,14,14,14,14,14,14,14,14,14,14,14,15,17,19,21,21,21])
+plt.ylabel(['Numero de Hermanos y Primos'])
+plt.xlabel(['Edad [a.]'])
+plt.title('Noe Guerrero Reyes')
+plt.savefig('temp.png')
+plt.show()
